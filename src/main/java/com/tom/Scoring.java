04ba84fbf42ce2b8198b9c06bcd54fd68afc7a6f
gyxnpy <liguan181@gmail.com>
2022-03-04 15:45:25 +0800

    score

---
+++ src/main/java/com/tom/Scoring.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Scoring {
+    public static void main(String[] args) {
+        int[] math={11,22,33,44};
+        int[] english={11,22,33,44};
+        String[] names={"jack","rose","tomas","hank"};
+        for(int i=0;i<=3;i++){
+            System.out.println(names[i]+"\t"+english[i]+"\t"+math[i]+"\t"+((english[i]+math[i]) /2));
+        }
+    }
+
+}

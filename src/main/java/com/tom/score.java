f9c482bd9d1e1df306e6b09387e8d4870a2b2d01
gyxnpy <liguan181@gmail.com>
2022-03-04 16:11:39 +0800

    ADD project Student

---
+++ src/main/java/com/tom/score.java
@@ -0,0 +1,11 @@
+package com.tom;
+
+public class score {
+    public static void main(String[] args) {
+        Student jack=new Student();
+        jack.name="jack";
+        jack.english=100;
+        jack.math=88;
+
+    }
+}

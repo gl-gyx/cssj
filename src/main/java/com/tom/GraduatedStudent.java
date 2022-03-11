72d765f636186b02d4b109ad6420c2e99de2bb0f
User <liguan181@gmail>
2022-03-11 16:33:19 +0800

    second class

---
+++ src/main/java/com/tom/GraduatedStudent.java
@@ -0,0 +1,12 @@
+package com.tom;
+
+public class GraduatedStudent extends Student{
+    int thesis;
+    public GraduatedStudent(String name,int english,int math,int thesis){
+        super(name,english,math);
+        this.thesis=thesis;
+    }
+    public void print(){
+        System.out.println(name+"\t"+english+"\t"+math+"\t"+getAverage()+"\t"+);
+    }
+}
--- src/main/java/com/tom/score.java
+++ src/main/java/com/tom/score.java
@@ -2,10 +2,15 @@ package com.tom;
 
 public class score {
     public static void main(String[] args) {
-        Student jack=new Student();
-        jack.name="jack";
+        Student jack=new Student("jack");
+        Student hank=new Student("hank",84,66);
         jack.english=100;
         jack.math=88;
-
+        jack.print();
+        hank.print();
+        Student s =new Student();
+        GraduatedStudent jane= new GraduatedStudent();
+        jane.name="jane";
+        jane.print();
     }
 }
